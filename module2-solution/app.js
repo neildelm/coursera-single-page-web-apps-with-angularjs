f4ba82d3648a9138b34bf67f048d6dafcfdf2336
Neil Delmer <neil.delmer@gmail.com>
2017-09-25 12:29:18 -0400

    Module 2 Soultion

---
+++ module2-solution/app.js
@@ -0,0 +1,56 @@
+(function () {
+'use strict';
+
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyShoppingController', ToBuyShoppingController)
+.controller('AlreadyBoughtShoppingController', AlreadyBoughtShoppingController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+ToBuyShoppingController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyShoppingController(ShoppingListCheckOffService) {
+  var itemsToBuy = this;
+  itemsToBuy.items = ShoppingListCheckOffService.getToBuyItems();
+
+  itemsToBuy.moveItem = function (key, item) {
+    ShoppingListCheckOffService.moveItem(key, item);
+  };
+
+}
+
+AlreadyBoughtShoppingController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtShoppingController(ShoppingListCheckOffService) {
+  var boughtCtrl = this;
+
+  boughtCtrl.items = ShoppingListCheckOffService.getBoughtItems();
+
+}
+
+function ShoppingListCheckOffService() {
+  var service = this;
+
+  var toBuyItems = [
+    { name: "cookies",         quantity: 10 },
+    { name: "chocolate stars", quantity: 6 },
+    { name: "ice creams",      quantity: 15 },
+    { name: "chips",           quantity: 12 },
+    { name: "milk shakes",     quantity: 20 }
+  ];
+
+  var boughtItems = [];
+
+  service.moveItem = function(key, item) {
+    boughtItems.push(item);
+    toBuyItems.splice(key, 1);
+  }
+
+  service.getToBuyItems = function() {
+    return toBuyItems;
+  }
+
+  service.getBoughtItems = function() {
+    return boughtItems;
+  }
+
+};
+
+})();
